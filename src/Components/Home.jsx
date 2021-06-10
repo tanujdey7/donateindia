cf73d6944b0de0c745a899f29356754c7b424512
Raghvendra Singh <Raghav26897@gmail.com>
2021-06-10 21:03:28 +0530

    dfsds

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Poverty from "../Images/poverty.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Home=()=> {
     return (
         <div className="home-wrapper">
             <div className="hero-section">
                 <div className="row">
-                    <div className="col"><h1>One</h1></div>
-                    <div className="col"><h1>Two</h1></div>
+                    <div className="col bg-info"><h1>One</h1></div>
+                    <div className="col bg-secondary">
+                        <div>
+                            <div className="card w-50">
+                                <div className="card-body">
+                                    <img src={Poverty} width="400" className="rounded" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
