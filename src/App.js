85c85e52c3f5c3416eae9881075c3b84614f1ad4
Raghvendra Singh <Raghav26897@gmail.com>
2021-06-10 11:51:45 +0530

    Home updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/HeadFoot/Navbar";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home=()=> {
+    return (
+        <div className="home-wrapper">
+            <div className="hero-section">
+                <div className="row">
+                    <div className="col"><h1>One</h1></div>
+                    <div className="col"><h1>Two</h1></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
