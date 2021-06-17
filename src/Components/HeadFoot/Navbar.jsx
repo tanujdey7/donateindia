1dbc8993b0adef85d35c44e25e9865b1cec0ac5c
Tanuj Dey <tanujdey7@gmail.com>
2021-06-17 13:28:08 +0530

    format

--- src/Components/HeadFoot/Navbar.jsx
+++ src/Components/HeadFoot/Navbar.jsx
@@ -17,21 +17,39 @@ const Navbar = () => {
 
         {show ? (
           <div className="ham-menu-div">
-          <div className="contact-div col">
-            <button className="btn btn-md"><h5 className="">Home</h5></button>
-            <button className="btn btn-md"><h5 className="">About Us</h5></button>
-            <button className="btn btn-md"><h5 className="">How it Works</h5></button>
-          </div>
+            <div className="contact-div col">
+              <button className="btn btn-md">
+                <h5 className="">Home</h5>
+              </button>
+              <button className="btn btn-md">
+                <h5 className="">About Us</h5>
+              </button>
+              <button className="btn btn-md">
+                <h5 className="">How it Works</h5>
+              </button>
+            </div>
 
             <div className="login-div col">
               <button className="donateus-btn btn btn-md ">Donate</button>
               <button className="donateus-btn btn btn-md ">Volunteer</button>
               <button className="login-btn btn btn-md">Login</button>
             </div>
-            
-          </div>) : null}
+          </div>
+        ) : null}
       </div>
-      {show ? (<CgClose className="close-icon" onClick={() => setShow(false)} size={50} />) : <GiHamburgerMenu className="hamburger-icon" onClick={() => setShow(true)} size={50} />}
+      {show ? (
+        <CgClose
+          className="close-icon"
+          onClick={() => setShow(false)}
+          size={50}
+        />
+      ) : (
+        <GiHamburgerMenu
+          className="hamburger-icon"
+          onClick={() => setShow(true)}
+          size={50}
+        />
+      )}
     </div>
   );
 };
