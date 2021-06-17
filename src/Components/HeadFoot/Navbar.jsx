ba93e959a0af496b795bd344231b97cc02b40a5b
Raghvendra Singh <32678356+Raghvendrars@users.noreply.github.com>
2021-06-17 13:20:00 +0530

    navbar edited

--- src/Components/HeadFoot/Navbar.jsx
+++ src/Components/HeadFoot/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";
 // import "./Navbar";
 import Logo from "../Images/logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { CgClose } from "react-icons/cg";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
@@ -16,22 +16,23 @@ const Navbar = () => {
         </div>
 
         {show ? (
-          <div className="ham-menu-div">
-          <div className="contact-div col">
-            <button className="btn btn-md"><h5 className="">Home</h5></button>
-            <button className="btn btn-md"><h5 className="">About Us</h5></button>
-            <button className="btn btn-md"><h5 className="">How it Works</h5></button>
-          </div>
+          <div className="ham-menu-div ">
+            <div className="contact-div col mt-3">
+              <button className="btn btn-md"><h5 className="">Home</h5></button>
+              <button className="btn btn-md"><h5 className="">About Us</h5></button>
+              <button className="btn btn-md"><h5 className="">How it Works</h5></button>
+            </div>
 
-            <div className="login-div col">
+            <div className="login-div col mt-3">
               <button className="donateus-btn btn btn-md ">Donate</button>
               <button className="donateus-btn btn btn-md ">Volunteer</button>
               <button className="login-btn btn btn-md">Login</button>
             </div>
-            
+
           </div>) : null}
       </div>
-      {show ? (<CgClose className="close-icon" onClick={() => setShow(false)} size={50} />) : <GiHamburgerMenu className="hamburger-icon" onClick={() => setShow(true)} size={50} />}
+      <div>
+        {show ? (<IoMdCloseCircle className="close-icon" onClick={() => setShow(false)} size={40} />) : <GiHamburgerMenu className="hamburger-icon" onClick={() => setShow(true)} size={40} />}</div>
     </div>
   );
 };
