40ccbcfebf66a26453717c4b586d2de5807dc4c0
Raghvendra Singh <32678356+Raghvendrars@users.noreply.github.com>
2021-06-18 12:40:12 +0530

    css edited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-// import logo from './logo.svg';
 import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Navbar from "./Components/HeadFoot/Navbar";
 import Home from "./Components/Home";
 import Footer from "./Components/HeadFoot/Footer";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
