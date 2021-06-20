acda952256146a23d7bdbad378fb8380285ba415
Raghvendra Singh <Raghav26897@gmail.com>
2021-06-21 00:29:00 +0530

    login sign up done

--- src/Pages/Loginpages/Login.jsx
+++ src/Pages/Loginpages/Login.jsx
@@ -1,30 +1,29 @@
 import React, { useState } from "react";
 import "./Login.css";
-import {FcGoogle} from "react-icons/fc";
-import {SiFacebook} from "react-icons/si";
+import { FcGoogle } from "react-icons/fc";
+import { SiFacebook } from "react-icons/si";
+import Donationpic from "../Images/logindonate.jpg";
 
 const Login = () => {
   const [state, setstate] = useState(false);
+  const [signup, setSignup] = useState(false);
 
   return (
     <div className="login-section">
       <div className="login-wrapper">
-        <div className="login-wrapper-row row">
-          <div className="login-title-wrap">
-            <div className="login-title-div">
-              <h1>Login to Your Account</h1>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s
-              </p>
-            </div>
-          </div>
-        </div>
         <div className="login-form-wrapper-row row">
           <div className="login-col-wrap">
             <div className="col">
-              <div className="login-form-div">
+              <img src={Donationpic} width="900" alt="donation pic" />
+            </div>
+
+            <div className="signup-form-col col">
+              {signup===false?(<div className="login-form-div">
+                <div className="login-head">
+                  <h1 className="login-title-text">Sign in</h1>
+                  <h5>New User?<button className="signup-into-btn btn-md" onClick={()=>setSignup(true)}>SignUp</button></h5>
+                </div>
+                <div className="whole-login-sec-wrap">
                 <div className="login-switch-btns">
                   <button
                     onClick={() => setstate(false)}
@@ -40,16 +39,16 @@ const Login = () => {
                   </button>
                 </div>
                 {state === false ? (
-                  <div className="donor-login-form">
+                  <div className="donor-login-form mt-2">
                     <form>
                       <input
                         type="text"
-                        className="form-control mt-3"
+                        className="form-controls mt-3"
                         placeholder="Enter email"
                       />
                       <input
                         type="text"
-                        className="form-control mt-3"
+                        className="form-controls mt-3"
                         placeholder="Enter pass"
                       />
                       <button type="submit" class="btn submit-donor mt-3">
@@ -63,12 +62,12 @@ const Login = () => {
                     <form>
                       <input
                         type="text"
-                        className="form-control mt-3"
+                        className="form-controls mt-3"
                         placeholder="Enter email"
                       />
                       <input
                         type="text"
-                        className="form-control mt-3"
+                        className="form-controls mt-3"
                         placeholder="Enter pass"
                       />
                       <button type="submit" class="btn btn-primary mt-2">
@@ -77,15 +76,93 @@ const Login = () => {
                     </form>
                   </div>
                 ) : null}
-
-                <div className="social-login-div mt-3">
-                    <button className="btn social-login-btn-google"><FcGoogle className="facebook-icon" size="25" /> Continue with Google</button>
-                    <button className="btn social-login-btn-facebook"><SiFacebook size="25" /> Continue with Facebook</button>
+                <div className="login-line mt-2">
+                    <hr></hr><h6 className="mt-1 ml-2 mr-2">or</h6><hr></hr>
                 </div>
-              </div>
-            </div>
-            <div className="col bg-secondary">
-              <h1>col 2</h1>
+                <div className="social-login-div mt-3">
+                  <button className="btn social-login-btn-google">
+                    <FcGoogle className="facebook-icon" size="25" /> Continue
+                    with Google
+                  </button>
+                  <button className="btn social-login-btn-facebook">
+                    <SiFacebook size="25" /> Continue with Facebook
+                  </button>
+                </div>
+                </div>
+              </div>)
+              :
+              <div className="login-form-div">
+                <div className="login-head">
+                  <h1 className="login-title-text">SignUp</h1>
+                  <h5>New User?<button className="signup-into-btn btn-md" onClick={()=>setSignup(false)}>Sign</button></h5>
+                </div>
+                <div className="whole-login-sec-wrap">
+                <div className="login-switch-btns">
+                  <button
+                    onClick={() => setstate(false)}
+                    className="btn login-switch-donor-btn"
+                  >
+                    I'm Donor
+                  </button>
+                  <button
+                    onClick={() => setstate(true)}
+                    className="btn login-switch-receiver-btn ml-3"
+                  >
+                    I'm Receiver
+                  </button>
+                </div>
+                {state === false ? (
+                  <div className="donor-login-form mt-2">
+                    <form>
+                      <input
+                        type="text"
+                        className="form-controls mt-3"
+                        placeholder="Enter email"
+                      />
+                      <input
+                        type="text"
+                        className="form-controls mt-3"
+                        placeholder="Enter pass"
+                      />
+                      <button type="submit" class="btn submit-donor mt-3">
+                        Submit Receiver
+                      </button>
+                    </form>
+                  </div>
+                ) : null}
+                {state ? (
+                  <div className="receiver-login-form">
+                    <form>
+                      <input
+                        type="text"
+                        className="form-controls mt-3"
+                        placeholder="Enter email"
+                      />
+                      <input
+                        type="text"
+                        className="form-controls mt-3"
+                        placeholder="Enter pass"
+                      />
+                      <button type="submit" class="btn btn-primary mt-2">
+                        Submit Receiver
+                      </button>
+                    </form>
+                  </div>
+                ) : null}
+                <div className="login-line mt-2">
+                    <hr></hr><h6 className="mt-1 ml-2 mr-2">or</h6><hr></hr>
+                </div>
+                <div className="social-login-div mt-3">
+                  <button className="btn social-login-btn-google">
+                    <FcGoogle className="facebook-icon" size="25" /> Continue
+                    with Google
+                  </button>
+                  <button className="btn social-login-btn-facebook">
+                    <SiFacebook size="25" /> Continue with Facebook
+                  </button>
+                </div>
+                </div>
+              </div>}
             </div>
           </div>
         </div>
