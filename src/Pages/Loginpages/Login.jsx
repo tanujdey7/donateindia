c1c06eda45c35a81a141592377153ffd37142f5d
Raghvendra Singh <Raghav26897@gmail.com>
2021-07-21 23:40:33 +0530

    login updated

--- src/Pages/Loginpages/Login.jsx
+++ src/Pages/Loginpages/Login.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { FcGoogle } from "react-icons/fc";
-import { SiFacebook } from "react-icons/si";
 import Donationpic from "../Images/logindonate.jpg";
-
 const Login = () => {
-  const [state, setstate] = useState(false);
   const [signup, setSignup] = useState(false);
   return (
     <div className="login-section">
@@ -13,125 +9,80 @@ const Login = () => {
         <div className="login-form-wrappe-col col">
           <img src={Donationpic} alt="donation pic" />
         </div>
-
         <div className="signup-form-col col">
           {signup === false ? (
             <div className="login-form-div">
               <div className="login-head">
-                <h1 className="login-title-text">Sign in</h1>
+                <h2 className="login-title-text">Sign in</h2>
                 <h5 className="switch-page-text">
                   New User?
                   <button
                     className="signup-into-btn btn-md"
                     onClick={() => setSignup(true)}
                   >
-                    SignUp
+                    <span className="h5">SignUp</span>
                   </button>
                 </h5>
               </div>
               <div className="whole-login-sec-wrap">
-                <div className="login-switch-btns" id="login-switch-btns">
-                  <button
-                    onClick={() => setstate(false)}
-                    className={`btn ${
-                      state == false ? "login-switch-donor-btn " : ""
-                    }${state ? "disabled" : ""}`}
-                  >
-                    I'm Donor
-                  </button>
-                  <button
-                    onClick={() => setstate(true)}
-                    className={`btn ${state ? "login-switch-donor-btn" : ""}${
-                      state == false ? "disabled" : ""
-                    }`}
-                  >
-                    I'm Receiver
-                  </button>
-                </div>
-                {state === false ? (
-                  <div className="donor-login-form">
-                    <form className="form-wrap">
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">
-                          Email address
-                        </label>
-                        <input
-                          type="email"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                        />
+                <div className="donor-login-form">
+                  <form className="form-wrap">
+                    <div class="input-group mb-2">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon3">
+                          Email
+                        </span>
                       </div>
-                      <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="basic-url"
+                        aria-describedby="basic-addon3"
+                      />
+                    </div>
+                    <small id="emailHelp" class="form-text text-muted">
+                      We'll never share your email with anyone else.
+                    </small>
+                    <div class="input-group mt-3 mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon3">
                           Password
-                        </label>
-                        <input
-                          type="password"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputPassword1"
-                        />
+                        </span>
                       </div>
-                      <div className="row w-100 ml-1">
-                        <button type="submit" class="btn submit-donor mt-3">
-                          Login
-                        </button>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="basic-url"
+                        aria-describedby="basic-addon3"
+                      />
+                    </div>
+                    <div class="input-group mt-2">
+                      <div className="row w-100">
+                        <div className="col-6">
+                          <div class="form-group form-check">
+                            <input
+                              type="checkbox"
+                              class="form-check-input"
+                              id="exampleCheck1"
+                            />
+                            <label class="form-check-label" for="exampleCheck1">
+                              Check me out
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-6">
+                          <a href="#" className="frgt-pass">
+                            Forget Password?
+                          </a>
+                        </div>
                       </div>
-                    </form>
-                  </div>
-                ) : null}
-                {state ? (
-                  <div className="receiver-login-form">
-                    <form className="form-wrap">
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">
-                          Email address
-                        </label>
-                        <input
-                          type="email"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputPassword1"
-                        />
-                      </div>
-                      <div className="row w-100 ml-1">
-                        <button type="submit" class="btn submit-donor mt-3">
-                          Login
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                ) : null}
-                <div className="login-line mt-3">
-                  <hr></hr>
-                  <h6 className="mt-1 ml-2 mr-2">Or Continue With</h6>
-                  <hr></hr>
-                </div>
-                <div className="social-login-div mt-3">
-                  <a className="btn social-login-btn-google">
-                    <h1 className="google-icon">
-                      <FcGoogle />
-                    </h1>
-                  </a>
-                  <a className="btn social-login-btn-facebook">
-                    <h1 className="facebook-icon">
-                      <SiFacebook />
-                    </h1>
-                  </a>
+                    </div>
+                    <div className="row w-100 ml-1">
+                      <button type="submit" class="btn submit-donor mt-3">
+                        Login
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
@@ -139,169 +90,73 @@ const Login = () => {
             // register section from here
             <div className="login-form-div">
               <div className="login-head">
-                <h1 className="login-title-text">SignUp</h1>
-                <h6>
+                <h2 className="login-title-text">SignUp</h2>
+                <h5>
                   Already Registered?
                   <button
                     className="signup-into-btn btn-md"
                     onClick={() => setSignup(false)}
                   >
-                    <span className="h5">Sign</span>
+                    <span className="h5">SignIn</span>
                   </button>
-                </h6>
+                </h5>
               </div>
               <div className="whole-login-sec-wrap">
-                <div className="login-switch-btns">
-                  <button
-                    onClick={() => setstate(false)}
-                    className={`btn ${
-                      state == false ? "login-switch-donor-btn " : ""
-                    }${state ? "disabled" : ""}`}
-                  >
-                    I'm Donor
-                  </button>
-                  <button
-                    onClick={() => setstate(true)}
-                    className="btn  ml-3"
-                    className={`btn ${state ? "login-switch-donor-btn" : ""}${
-                      state == false ? "disabled" : ""
-                    }`}
-                  >
-                    I'm Receiver
-                  </button>
-                </div>
-                {state === false ? (
-                  <div className="donor-login-form">
-                    <form className="form-wrap">
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">
-                          Email address
-                        </label>
-                        <input
-                          type="email"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">
-                          Phone Number
-                        </label>
-                        <input
-                          type="tel"
-                          class="form-control border-0"
-                          id="exampleInputEmail1"
-                          required
-                          aria-describedby="emailHelp"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputPassword1"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">
-                          Confirm Password
-                        </label>
-                        <input
-                          type="password"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                        />
-                      </div>
-                      <div className="row w-100 ml-1">
-                        <button type="submit" class="btn submit-donor mt-3">
-                          Register
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                ) : null}
-                {state ? (
-                  <div className="receiver-login-form">
-                    <form className="form-wrap">
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">
-                          Email address
-                        </label>
-                        <input
-                          type="email"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">
-                          Phone Number
-                        </label>
-                        <input
-                          type="tel"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          required
-                          class="form-control border-0"
-                          id="exampleInputPassword1"
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">
-                          Confirm Password
-                        </label>
-                        <input
-                          type="password"
-                          class="form-control border-0"
-                          id="exampleInputEmail1"
-                          required
-                          aria-describedby="emailHelp"
-                        />
-                      </div>
-                      <div className="row w-100 ml-1">
-                        <button type="submit" class="btn submit-donor mt-3">
-                          Register
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                ) : null}
-                <div className="login-line mt-3">
-                  <hr></hr>
-                  <h6 className="mt-1 ml-2 mr-2">Or Continue With</h6>
-                  <hr></hr>
-                </div>
-                <div className="social-login-div mt-3">
-                  <a className="btn social-login-btn-google">
-                    <h1 className="google-icon">
-                      <FcGoogle />
-                    </h1>
-                  </a>
-                  <a className="btn social-login-btn-facebook">
-                    <h1 className="facebook-icon">
-                      <SiFacebook />
-                    </h1>
-                  </a>
+                <div className="donor-login-form">
+                  <form className="form-wrap">
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">
+                        Email address
+                      </label>
+                      <input
+                        type="email"
+                        required
+                        class="form-control "
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                      />
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">
+                        Phone Number
+                      </label>
+                      <input
+                        type="tel"
+                        class="form-control"
+                        id="exampleInputEmail1"
+                        required
+                        aria-describedby="emailHelp"
+                      />
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleInputPassword1" class="form-label">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        required
+                        class="form-control"
+                        id="exampleInputPassword1"
+                      />
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">
+                        Confirm Password
+                      </label>
+                      <input
+                        type="password"
+                        required
+                        class="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                      />
+                    </div>
+                    <div className="row w-100 ml-1">
+                      <button type="submit" class="btn submit-donor mt-3">
+                        Register
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
